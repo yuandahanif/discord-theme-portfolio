ca1411a812ecc77916d781b47961ed3bae1f1245
yuanda <34027873+yuandahanif@users.noreply.github.com>
2022-02-18 06:50:25 +0700

    add server json data

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,5 +1,6 @@
 import ServerAvatar from "components/server/serverAvatar";
 import { Outlet } from "react-router-dom";
+import serverData from "../data/server.json";
 
 const Layout = () => {
   return (
@@ -13,11 +14,21 @@ const Layout = () => {
                   <ServerAvatar name="Home" to="/dm" />
                   <div className="mt-2 w-6 mx-auto border-b-2 border-gray-700"></div>
                 </li>
+                {serverData.map((server) => (
+                  <li key={server.id}>
+                    <ServerAvatar
+                      image={server.image_url}
+                      name={server.name}
+                      to={`/server/${server.id}`}
+                    />
+                  </li>
+                ))}
                 <li>
-                  <ServerAvatar name="Portfolio" to="/server/portfolio" />
-                </li>
-                <li>
-                  <ServerAvatar name="404" to="/404" />
+                  <ServerAvatar
+                    image="https://ui-avatars.com/api/?name=404"
+                    name="404"
+                    to={`/404`}
+                  />
                 </li>
               </ul>
             </nav>
