7155d86507644fb1ea141513f7717277f80d9260
yuanda <34027873+yuandahanif@users.noreply.github.com>
2022-02-17 10:47:08 +0700

    add tailwind color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dc: {
+          black: {
+            700: "#202225",
+            500: "#2F3136",
+            600: "#32353B",
+            100: "#36393F",
+          },
+          text: {
+            base: "#8E9297",
+            light: "#B9BBBE",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
